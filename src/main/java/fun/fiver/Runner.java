d75021305f4fa17f79236175721ac7b3e59f2092
Fatih İver <fatih.iver@boun.edu.tr>
2020-05-23 22:20:57 +0300

    initialize project

---
+++ src/main/java/fun/fiver/Runner.java
@@ -0,0 +1,18 @@
+package fun.fiver;
+
+import fun.fiver.SomeSmallTest;
+import org.graphwalker.java.test.Result;
+import org.graphwalker.java.test.TestExecutor;
+
+import java.io.IOException;
+
+public class Runner {
+  public static void main(String[] args) throws IOException {
+    TestExecutor executor = new TestExecutor(
+      SomeSmallTest.class
+    );
+
+    Result result = executor.execute(true);
+    System.out.println("Done: [" + result.getResults().toString(2) + "]");
+  }
+}
